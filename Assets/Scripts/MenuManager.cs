972ced9eabc174804b930dab30d040af5692306c
Atlinux <atlinuxtm@gmail.com>
2019-01-26 14:54:14 -0500

    Menu Manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour, IService
+{
+    public GameObject[] menus;
+    [SerializeField]
+    private SaveManager saveManager;
+    [SerializeField]
+    private TextMeshProUGUI pointsToWinText;
+    [SerializeField]
+    private TextMeshProUGUI gameModeText;
+
+    private void Awake()
+    {
+        ServiceLocator.Instance.AddService(this);
+    }
+
+    private void Start()
+    {
+        saveManager = ServiceLocator.Instance.GetService<SaveManager>();
+    }
+
+    private void OnDestroy()
+    {
+        ServiceLocator.Instance.RemoveService<MenuManager>();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameplaySetup");
+    }
+
+    public void GotoMenu(string name)
+    {
+        for (int i = 0; i < menus.Length; i++)
+            menus[i].SetActive(menus[i].name == name);
+    }
+}
---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TransitionManager : MonoBehaviour, IService
+{
+    [Header("Dependencies")]
+    public Image fillImage;
+
+    [Header("Misc")]
+	public bool inTransition = false;
+    [SerializeField]
+	private AnimationCurve curve;
+
+	private float minimumLogoTime = 1.0f; //Minimum time of the preloader
+
+    public void Awake()
+    {
+        ServiceLocator.Instance.AddService(this);
+    }
+
+    public void Start()
+    {
+        StartCoroutine(PreloadFade());
+	}
+
+	public IEnumerator PreloadFade() {
+		//Stalls logo for minimumLogoTime if the preload is longer than the minimumLogoTime
+		if (Time.time < minimumLogoTime)
+			yield return new WaitForSeconds (minimumLogoTime);
+		else
+			yield return new WaitForSeconds (Time.time);
+
+		TransitionTo ("Menu");
+	}
+
+    public void TransitionTo(string scene)
+    {
+        TransitionTo(scene, 0.25f);
+    }
+
+    public void TransitionTo(string scene, float delay) {
+		if (!inTransition)
+			StartCoroutine(TransitionEnum (scene, delay));
+	}
+
+    private IEnumerator TransitionEnum(string scene, float delay) {
+        fillImage.gameObject.SetActive(true);
+        inTransition = true;
+
+        float time = 0;
+        while (time <= delay)
+        {
+            fillImage.fillAmount = curve.Evaluate(time / delay);
+            yield return new WaitForEndOfFrame();
+            time += Time.deltaTime;
+        }
+        
+        AsyncOperation loadingOperation = SceneManager.LoadSceneAsync(scene);
+
+        while (!loadingOperation.isDone)
+            yield return new WaitForEndOfFrame();
+
+        time = delay;
+        while (time >= 0)
+        {
+            fillImage.fillAmount = curve.Evaluate(time / delay);
+            yield return new WaitForEndOfFrame();
+            time -= Time.deltaTime;
+        }
+        
+        inTransition = false;
+        fillImage.gameObject.SetActive(false);
+    }
+}
