9eefd9a9712488c3b367ca2f5f2eac079fe589af
Atlinux <atlinuxtm@gmail.com>
2019-01-27 16:16:45 -0500

    a

---
+++ Assets/Scripts/BoomerangEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangEnemy : EnemyController
+{
+    public bool fired;
+    public float reloadTime;
+    public GameObject projectilePrefab;
+    public ParticleSystem muzzleFlash;
+    public AudioSource muzzleAudio;
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+            rb.velocity = transform.forward * speed;
+            rb.MoveRotation(Quaternion.Euler(new Vector3(0, -Mathf.Atan2(target.position.z - transform.position.z, target.position.x - transform.position.x) * Mathf.Rad2Deg + 90, 0)));
+            if (!fired)
+                StartCoroutine(Fire());
+        }
+    }
+
+    private IEnumerator Fire()
+    {
+        fired = true;
+        muzzleFlash.Play();
+        muzzleAudio.Play();
+        Instantiate(projectilePrefab, transform.position, Quaternion.Euler(transform.eulerAngles + new Vector3(0, 0, 0))).GetComponent<BoomerangProjectile>().center = transform;
+        yield return new WaitForSeconds(reloadTime);
+        fired = false;
+    }
+}
---
+++ Assets/Scripts/BoomerangProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangProjectile : Projectile
+{
+    public Rigidbody rb;
+    public float height;
+    public float width;
+    public float speed;
+    public float time;
+    public Transform center;
+    public ParticleSystem deathParticle;
+    public AudioSource deathSound;
+
+    private void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    public void Update()
+    {
+        time += Time.deltaTime * speed;
+        transform.position = new Vector3(center.position.x + (width * Mathf.Cos(Mathf.Deg2Rad * time)),
+            center.position.y + (height * Mathf.Sin(Mathf.Deg2Rad * time)));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Projectiles") || other.CompareTag("Enemy"))
+            return;
+        if (other.gameObject.CompareTag("Player"))
+            other.gameObject.GetComponent<PlayerController>().Damage(damage);
+        deathParticle.transform.SetParent(null);
+        deathParticle.Play();
+        deathSound.Play();
+        Destroy(deathParticle.gameObject, deathParticle.main.duration);
+        Destroy(gameObject);
+    }
+}
